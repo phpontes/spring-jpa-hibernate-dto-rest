d7f7ede7e8485cdb76515d5bbe04b8f8029ad2df
phpontes <ppontes95@hotmail.com>
2024-12-05 13:40:20 -0300

    Implementing POST, PUT and DELETE

--- src/main/java/com/paulopontes/portfolio/controller/ProductController.java
+++ src/main/java/com/paulopontes/portfolio/controller/ProductController.java
@@ -3,8 +3,12 @@ package com.paulopontes.portfolio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,20 @@ public class ProductController {
 	@GetMapping("/{id}")
 	public ProductDTO getProductById(@PathVariable Long id) {
 		return productService.getProductById(id);
+	}
+	
+	@PostMapping
+	public ProductDTO createProduct(@RequestBody ProductDTO productDTO) {
+		return productService.createProduct(productDTO);
+	}
+	
+	@PutMapping("/{id}")
+	public ProductDTO updateProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+		return productService.updateProduct(id, productDTO);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteProduct(@PathVariable Long id) {
+		productService.deleteProduct(id);
 	}
 }
--- src/main/java/com/paulopontes/portfolio/service/ProductService.java
+++ src/main/java/com/paulopontes/portfolio/service/ProductService.java
@@ -32,4 +32,31 @@ public class ProductService {
 		dto.setPrice(product.getPrice());
 		return dto;
 	}
+
+	public ProductDTO createProduct(ProductDTO productDTO) {
+		Product product = toEntity(productDTO);
+		Product savedProduct = productRepository.save(product);
+		return toDTO(savedProduct);
+	}
+
+	private Product toEntity(ProductDTO dto) {
+		Product product = new Product();
+		product.setId(dto.getId());
+		product.setName(dto.getName());
+		product.setPrice(dto.getPrice());
+		return product;
+	}
+
+	public ProductDTO updateProduct(Long id, ProductDTO productDTO) {
+		return productRepository.findById(id).map(product -> {
+			product.setName(productDTO.getName());
+			product.setPrice(productDTO.getPrice());
+			Product updatedProduct = productRepository.save(product);
+			return toDTO(updatedProduct);
+		}).orElse(null);
+	}
+
+	public void deleteProduct(Long id) {
+		productRepository.deleteById(id);
+	}
 }
